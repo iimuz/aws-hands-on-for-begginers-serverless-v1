461ca6603f7ac73c3032847acefc7f636a6a2525
iimuz <iz.primary.story+github@gmail.com>
2023-01-09 12:15:49 +0000

    fix: api gatewayの連携に利用するコードに修正

--- src/til-20221127/hands-on-serverless-2/translate-function/translate-function.py
+++ src/til-20221127/hands-on-serverless-2/translate-function/translate-function.py
@@ -2,16 +2,16 @@
 import json
 import boto3
 
-translate = boto3.client('translate')
+translate = boto3.client(service_name='translate')
 
 def lambda_handler(event, context):
 
-    input_text = "こんにちは"
+    input_text = event['queryStringParameters']['input_text']
 
     response = translate.translate_text(
         Text=input_text,
-        SourceLanguageCode='ja',
-        TargetLanguageCode='en'
+        SourceLanguageCode="ja",
+        TargetLanguageCode="en"
     )
 
     output_text = response.get('TranslatedText')
@@ -20,5 +20,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps({
             'output_text': output_text
-        })
+        }),
+        'isBase64Encoded': False,
+        'headers': {}
     }
